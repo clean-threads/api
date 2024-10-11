69141c34b94d287a168060b8919aa098f89c86bc
Julie Chan <juliechan03@gmail.com>
2024-10-10 22:31:49 -0700

    boilerplate code

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, origins='*')
+
+# API placeholder
+@app.route("/api/users", methods=["GET"])
+def users():
+    return jsonify(
+        {
+            "users": [
+                'Alice', 'Bob', 'Charlie', 'David', 'Eve'
+            ]
+        }
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
